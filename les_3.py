38e7bd5b7a5e37d18acb8ed181a499ba18935f8a
unknown <radivoniknikita@gmail.com>
2022-09-30 21:28:19 +0300

    дз

---
+++ les_3.py
@@ -0,0 +1,17 @@
+# Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+
+# Пример:
+
+# - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}---неправильный
+from cgi import print_environ
+import fractions
+from tkinter import N
+number = float(input("Введите число для последовательности: "))
+step=float
+list=[1,step]
+for i in range(1,int(number)):
+    step=round((1+1/number)**number,1)
+    number+=1
+    list=[1,step]
+    print(list)
+    
