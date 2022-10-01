dc531c726bf328d3f2d35473ed20f1511c1afe37
unknown <radivoniknikita@gmail.com>
2022-10-01 11:39:37 +0300

    дз

---
+++ les_5.py
@@ -0,0 +1,9 @@
+#перемешивание списка
+import random
+list = [11, 2, 23, 34, 55, 16]
+for i in range(len(list)):
+    a =  random.randint(0,len(list) -1)
+    if list[i] != list[a]:
+        list[i],list[a] = list[a],list[i]
+        print(list[i], list[a])
+print(list)
