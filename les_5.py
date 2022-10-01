7db054ec28131039ff95facb49656b0bdbf7fff1
unknown <radivoniknikita@gmail.com>
2022-10-01 11:41:36 +0300

    дз

--- les_5.py
+++ les_5.py
@@ -3,7 +3,7 @@ import random
 list = [11, 2, 23, 34, 55, 16]
 for i in range(len(list)):
     a =  random.randint(0,len(list) -1)
-    if list[i] != list[a]:
+    if i != a:
         list[i],list[a] = list[a],list[i]
         print(list[i], list[a])
 print(list)
