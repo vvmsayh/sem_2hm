dafdf1e15f8b5566e7ac6ad1fd133a379008d15a
unknown <radivoniknikita@gmail.com>
2022-10-01 11:07:51 +0300

    дз

--- les_1.py
+++ les_1.py
@@ -4,25 +4,42 @@
 
 # - 6782 -> 23
 # - 0,56 -> 11
-from unittest import result
+# from unittest import result
 
 
-number = float(input("введите вещественное число: "))
+# number = float(input("введите вещественное число: "))
 
 
-def remove_dot_from_float(n):
-    while n % 10 != 0:
-        n = round(n * 10, 7)
-    return int(n // 10)
+# def remove_dot_from_float(n):
+#     while n % 10 != 0:
+#         n = round(n * 10, 7)
+#     return int(n // 10)
 
 
-num_int = remove_dot_from_float(number)
+# num_int = remove_dot_from_float(number)
 
 
 
-result=0
-while number>0:
-    result += number % 10
-    number //= 10
-print(round(result))
+# result=0
+# while number>0:
+#     result += number % 10
+#     number //= 10
+# print(round(result))
+
+n = input("Введите число = ").replace('.', '')
+while not n.isdigit():
+    n = input("Введите число = ").replace('.', '')
+    
+my_sum = 0
+# for i in s:
+#     my_sum += int(i)
+s = list(n3)
+print(map(int,s))
+my_sum = sum(list(map(int,s)))
+print(my_sum)
+
+
+# num = input('Введите вещественное число: ')
+# while not num.lstrip('-').replace('.', '',1).isdigit(): num = input('Введите вещественное число: ')  
+# print(sum(list(map(int, num.lstrip('-').replace('.', '', 1)))))
 
